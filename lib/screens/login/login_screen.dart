258a8cb870c7d67723af72a349bba1d3376ffb72
seayurre <yulyul03@daum.net>
2025-06-24 03:02:43 +0900

    make splashscreen(loading screen)

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Text("로그인스크린입니다요~"),
+      ),
+    );
+  }
+}
