1a7dc0596b0ed6e9600596e8f7d2070d09a989e9
seayurre <yulyul03@daum.net>
2025-06-27 13:52:24 +0900

    making mainview... (not complete)

---
+++ lib/screens/patients/mainview/graphview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GraphView extends StatefulWidget {
+  const GraphView({super.key});
+
+  @override
+  State<GraphView> createState() => _GraphViewState();
+}
+
+class _GraphViewState extends State<GraphView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
