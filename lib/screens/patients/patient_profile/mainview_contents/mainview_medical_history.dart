e1d635481303148198c3eb8a1ad2d44e995ae8c3
seayurre <yulyul03@daum.net>
2025-06-28 23:38:56 +0900

    refactor mainview_medical_history

--- lib/screens/patients/patient_profile/mainview_contents/mainview_medical_history.dart
+++ lib/screens/patients/patient_profile/mainview_contents/mainview_medical_history.dart
@@ -5,6 +5,8 @@ import 'package:piethon_team5_fe/widgets/gaps.dart';
 import 'package:piethon_team5_fe/widgets/maincolors.dart';
 import 'package:timelines_plus/timelines_plus.dart';
 
+enum HistoryType { condition, medication, imaging, labResult, surgery, clinicalNotes }
+
 class MainviewMedicalHistory extends StatefulWidget {
   const MainviewMedicalHistory({super.key});
 
@@ -18,38 +20,39 @@ class _MainviewMedicalHistoryState extends State<MainviewMedicalHistory> {
       date: 'June 27, 2025',
       title: 'Hypertension Assessment',
       description: 'Blood pressure: 140/90 mmHg. Diagnosed with Stage 1 Hypertension. Regular monitoring required.',
-      icon: Symbols.cardiology,
-      iconBackgroundColor: const Color(0xFF7F1D1D),
-      tags: ['Condition', 'Monitoring'],
-      iconTextColor: const Color(0xFFEF4444),
+      tags: ['Monitoring'],
+      historyType: HistoryType.condition,
     ),
     TimelineEvent(
-      date: 'June 20, 2025',
-      title: 'Prescription Update',
-      description: 'Prescribed Lisinopril 10mg daily for blood pressure management. Review in 3 months.',
-      icon: Icons.medication_outlined,
-      tags: ['Medication', 'New Prescription'],
-      iconTextColor: const Color(0xFFF43F5E),
-      iconBackgroundColor: const Color(0xFF881337),
-    ),
+        date: 'June 20, 2025',
+        title: 'Prescription Update',
+        description: 'Prescribed Lisinopril 10mg daily for blood pressure management. Review in 3 months.',
+        tags: ['New Prescription'],
+        historyType: HistoryType.medication),
     TimelineEvent(
-      date: 'June 14, 2025',
-      title: 'Chest MRI',
-      description: 'MRI revealed a 1.8 cm nodule in the right upper lobe. Follow-up recommended in 3 months.',
-      icon: Icons.image_outlined,
-      iconBackgroundColor: const Color(0xFF713F12),
-      tags: ['Imaging', 'Follow-up Required'],
-      iconTextColor: const Color(0xFFEAB308),
-    ),
+        date: 'June 14, 2025',
+        title: 'Chest MRI',
+        description: 'MRI revealed a 1.8 cm nodule in the right upper lobe. Follow-up recommended in 3 months.',
+        tags: ['Follow-up Required'],
+        historyType: HistoryType.imaging),
     TimelineEvent(
-      date: 'June 14, 2025',
-      title: 'Blood Work Results',
-      description: 'Complete blood count and metabolic panel completed. Elevated white blood cell count noted.',
-      icon: Icons.biotech_outlined,
-      iconBackgroundColor: const Color(0xFF581C87),
-      iconTextColor: const Color(0xFFA855F7),
-      tags: ['Lab Result', 'Abnormal'],
-    ),
+        date: 'June 14, 2025',
+        title: 'Blood Work Results',
+        description: 'Complete blood count and metabolic panel completed. Elevated white blood cell count noted.',
+        tags: ['Abnormal'],
+        historyType: HistoryType.labResult),
+    TimelineEvent(
+        date: 'May 15, 2025',
+        title: 'Appendectomy',
+        description: 'Laparoscopic appendectomy performed. No complications. Recovery progressing well.',
+        tags: ['Completed'],
+        historyType: HistoryType.surgery),
+    TimelineEvent(
+        date: 'May 2, 2025',
+        title: 'Chest CT Scan',
+        description: 'CT scan showed a 1.4 cm nodule in the right upper lobe. Additional imaging recommended.',
+        tags: ['Follow-up Required'],
+        historyType: HistoryType.clinicalNotes),
   ];
 
   @override
@@ -130,10 +133,43 @@ class _MainviewMedicalHistoryState extends State<MainviewMedicalHistory> {
               Container(
                 padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  color: event.iconBackgroundColor,
+                  color: event.historyType == HistoryType.condition
+                      ? const Color(0xFF7F1D1D)
+                      : event.historyType == HistoryType.medication
+                          ? const Color(0xFF881337)
+                          : event.historyType == HistoryType.imaging
+                              ? const Color(0xFF713F12)
+                              : event.historyType == HistoryType.labResult
+                                  ? const Color(0xFF581C87)
+                                  : event.historyType == HistoryType.surgery
+                                      ? const Color(0xFF14532D)
+                                      : const Color(0xFF01276D),
                   borderRadius: BorderRadius.circular(8),
                 ),
-                child: Icon(event.icon, color: event.iconTextColor, size: 24),
+                child: Icon(
+                    event.historyType == HistoryType.condition
+                        ? Symbols.cardiology
+                        : event.historyType == HistoryType.medication
+                            ? Icons.medication_outlined
+                            : event.historyType == HistoryType.imaging
+                                ? Icons.image_outlined
+                                : event.historyType == HistoryType.labResult
+                                    ? Icons.biotech_outlined
+                                    : event.historyType == HistoryType.surgery
+                                        ? Icons.masks_outlined
+                                        : Symbols.docs,
+                    color: event.historyType == HistoryType.condition
+                        ? const Color(0xFFEF4444)
+                        : event.historyType == HistoryType.medication
+                            ? const Color(0xFFF43F5E)
+                            : event.historyType == HistoryType.imaging
+                                ? const Color(0xFFEAB308)
+                                : event.historyType == HistoryType.labResult
+                                    ? const Color(0xFFA855F7)
+                                    : event.historyType == HistoryType.surgery
+                                        ? const Color(0xFF22C55E)
+                                        : const Color(0xFF1767F9),
+                    size: 24),
               ),
               const SizedBox(width: 12),
               Expanded(
@@ -163,16 +199,56 @@ class _MainviewMedicalHistoryState extends State<MainviewMedicalHistory> {
           Wrap(
             spacing: 8.0,
             runSpacing: 4.0,
-            children: event.tags.map((tag) {
-              final bool isFirst = (event.tags.indexOf(tag) == 0); //첫 번째 태그만 다른 색깔로
-              return Container(
+            children: [
+              Container(
                 decoration: BoxDecoration(
-                    color: isFirst ? event.iconBackgroundColor : MainColors.dividerLine, borderRadius: BorderRadius.circular(20)),
+                    color: event.historyType == HistoryType.condition
+                        ? const Color(0xFF7F1D1D)
+                        : event.historyType == HistoryType.medication
+                            ? const Color(0xFF881337)
+                            : event.historyType == HistoryType.imaging
+                                ? const Color(0xFF713F12)
+                                : event.historyType == HistoryType.labResult
+                                    ? const Color(0xFF581C87)
+                                    : event.historyType == HistoryType.surgery
+                                        ? const Color(0xFF14532D)
+                                        : const Color(0xFF01276D),
+                    borderRadius: BorderRadius.circular(20)),
                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                child:
-                    Text(tag, style: TextStyle(color: isFirst ? event.iconTextColor : MainColors.sidebarItemText, fontSize: 12)),
-              );
-            }).toList(),
+                child: Text(
+                    event.historyType == HistoryType.condition
+                        ? 'Condition'
+                        : event.historyType == HistoryType.medication
+                            ? 'Medication'
+                            : event.historyType == HistoryType.imaging
+                                ? 'Imaging'
+                                : event.historyType == HistoryType.labResult
+                                    ? 'Lab Result'
+                                    : event.historyType == HistoryType.surgery
+                                        ? 'Surgery'
+                                        : 'Clinical Notes',
+                    style: TextStyle(
+                        color: event.historyType == HistoryType.condition
+                            ? const Color(0xFFEF4444)
+                            : event.historyType == HistoryType.medication
+                                ? const Color(0xFFF43F5E)
+                                : event.historyType == HistoryType.imaging
+                                    ? const Color(0xFFEAB308)
+                                    : event.historyType == HistoryType.labResult
+                                        ? const Color(0xFFA855F7)
+                                        : event.historyType == HistoryType.surgery
+                                            ? const Color(0xFF22C55E)
+                                            : const Color(0xFF1767F9),
+                        fontSize: 12)),
+              ),
+              ...event.tags.map((tag) {
+                return Container(
+                  decoration: BoxDecoration(color: MainColors.dividerLine, borderRadius: BorderRadius.circular(20)),
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  child: Text(tag, style: const TextStyle(color: MainColors.sidebarItemText, fontSize: 12)),
+                );
+              })
+            ],
           ),
         ],
       ),
@@ -185,18 +261,14 @@ class TimelineEvent {
   final String date;
   final String title;
   final String description;
-  final IconData icon;
-  final Color iconBackgroundColor;
-  final Color iconTextColor;
   final List<String> tags;
+  final HistoryType historyType;
 
   TimelineEvent({
     required this.date,
     required this.title,
     required this.description,
-    required this.icon,
-    required this.iconBackgroundColor,
-    required this.iconTextColor,
     required this.tags,
+    required this.historyType,
   });
 }
